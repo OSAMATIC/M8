ce2199d6866c98bfadb100f10ce434056508e795
OSAMATIC <2023_osama.belaatar@iticbcn.cat>
2025-10-20 22:11:32 +0200

    haciendo pull desde casa para el trabajo del cole  me falla .idea

--- activitat4-m8-02/app/src/main/java/com/example/activitat_04_m8_02/variables.kt
+++ activitat4-m8-02/app/src/main/java/com/example/activitat_04_m8_02/variables.kt
@@ -1,5 +1,45 @@
 package com.example.activitat_04_m8_02
 
 fun main (){
+    var age = 1
+    val ano:Int = 0
+
+    val a:Long = 30
+
+    //Float
+    val floatExample:Float = 33.5f
+
+    val charExample:Char = 'a'
+    val stringExample:String = "mecagoenti"
+
+    //variables booleanas
+
+    val booleanExample:Boolean = false
+    val hola = true
+
+    // esto osn valores
+  /*  fun getAge(age:Int){
+        if (age == 1){
+            println("enano")
+        } else if (age >=2 ){
+                println("hola"  )
+            }else{
+                println("adeu")
+            }
+    }   */
+
+    fun getAge(age:Int){
+        when(age){
+            1 -> print("joven")
+            2 -> println("joven")
+            3-> print("joven casi joven")
+            4 -> {  print("PROMPT")
+                print("uuh")
+            }
+         }
+    }
+
+
+
 
 }
